3255dc2e9671571858a3b42034e3f076a154bbfa
sameermd16 <sameerthemc@gmail.com>
2023-06-25 23:31:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//object destructuring
+
+const hero = {
+   elementId: 'hero',
+   name: 'Wizard',
+   avatar: "images/wizard.png",
+   health: 60,
+   diceRoll: 6
+}
+const monster = {
+   elementId: 'monster',
+   name: 'Orc',
+   avatar: "images/orc.png",
+   health: 10,
+   diceRoll: 4
+}
+
+function renderCharacter(data) {
+
+   document.getElementById(`${data.elementId}`).innerHTML += `
+      <div class="character-card">
+         <h4 class="name"> ${data.name} </h4>
+         <img class="avatar" src="${data.avatar}"/>
+         <p class="health">health: <b> ${data.health} </b></p>
+         <div class="dice-container">
+            <div class="dice"> ${data.diceRoll} </div>
+         </div>
+      </div>  
+   `
+}
+
+renderCharacter(hero)
+renderCharacter(monster)
+
